f30020c1a57fe93407d63dba4123ceee3f2cb8f4
bsag <butshesagirl@rousette.org.uk>
2023-07-08 11:44:12 +0100

    Trying to fix bottom row mods
    I think that the keypos_def layout was throwing off the bottom row of combos
    as the pinky keys were effectively defined as thumbs for combos, despite
    being on the bottow row of the keymap.

--- zmk-nodefree-config/keypos_def/keypos_38keys.h
+++ zmk-nodefree-config/keypos_def/keypos_38keys.h
@@ -1,12 +1,13 @@
 // 3*5+3 plus 1 pinky key, like TOTEM
-/*                              38 KEY MATRIX / LAYOUT MAPPING
+/* 38 KEY MATRIX / LAYOUT MAPPING
 
-  ╭────────────────────┬────────────────────╮ ╭─────────────────────┬─────────────────────╮
-  │  0   1   2   3   4 │  5   6   7   8   9 │ │ LT4 LT3 LT2 LT1 LT0 │ RT0 RT1 RT2 RT3 RT4 │
-  │ 10  11  12  13  14 │ 15  16  17  18  19 │ │ LM4 LM3 LM2 LM1 LM0 │ RM0 RM1 RM2 RM3 RM4 │
-  │ 20  21  22  23  24 │ 25  26  27  28  29 │ │ LB4 LB3 LB2 LB1 LB0 │ RB0 RB1 RB2 RB3 RB4 │
-  ╰─30────╮ 31  32  33 │ 34  35  36 ╭────37─╯ ╰─LH4───╮ LH2 LH1 LH0 │ RH0 RH1 RH2 ╭───RH4─╯
-          ╰────────────┴────────────╯                 ╰─────────────┴─────────────╯             */
+   ╭────────────────────┬────────────────────╮     ╭─────────────────────┬─────────────────────╮
+   │  0   1   2   3   4 │  5   6   7   8   9 │     │ LT4 LT3 LT2 LT1 LT0 │ RT0 RT1 RT2 RT3 RT4 │
+   │ 10  11  12  13  14 │ 15  16  17  18  19 │     │ LM4 LM3 LM2 LM1 LM0 │ RM0 RM1 RM2 RM3 RM4 │
+  20 21  22  23  24  25 │ 26  27  28  29  30 31  LB5 LB4 LB3 LB2 LB1 LB0 │ RB0 RB1 RB2 RB3 RB4 RB5
+   ╰───────╮ 32  33  34 │ 35  36  37 ╭───────╯     ╰───────╮ LH2 LH1 LH0 │ RH0 RH1 RH2 ╭───────╯
+           ╰────────────┴────────────╯                     ╰─────────────┴─────────────╯             */
+
 
 #pragma once
 
@@ -34,24 +35,24 @@
 #define RM3 18
 #define RM4 19
 
-#define LB0 24  // left-bottom row
-#define LB1 23
-#define LB2 22
-#define LB3 21
-#define LB4 20
+#define LB0 25  // left-bottom row
+#define LB1 24
+#define LB2 23
+#define LB3 22
+#define LB4 21
+#define LH4 20  // left outer pinky
 
-#define RB0 25  // right-bottom row
-#define RB1 26
-#define RB2 27
-#define RB3 28
-#define RB4 29
+#define RB0 26  // right-bottom row
+#define RB1 27
+#define RB2 28
+#define RB3 29
+#define RB4 30
+#define RH4 31  // right outer pinky
 
-#define LH0 33  // left thumb keys
-#define LH1 32
-#define LH2 31
-#define LH4 30  // left outer pinky
+#define LH0 34  // left thumb keys
+#define LH1 33
+#define LH2 32
 
-#define RH0 34  // right thumb keys
-#define RH1 35
-#define RH2 36
-#define RH4 37  // right outer pinky
+#define RH0 35  // right thumb keys
+#define RH1 36
+#define RH2 37
